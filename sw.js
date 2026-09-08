66091d061c6a325cbb88130cb65590cce4d08537
Site Kodal <wahyusan754@gmail.com>
2026-09-08 19:28:43 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -11,8 +11,8 @@
 //     begitu dapat jaringan.
 // ══════════════════════════════════════════════════════════
 
-const SW_VERSION    = 'p2h-sw-v4';
-const CACHE_NAME     = 'p2h-shell-v4'; // NAIKKAN versi ini tiap kali app di-update & redeploy,
+const SW_VERSION    = 'p2h-sw-v5';
+const CACHE_NAME     = 'p2h-shell-v5'; // NAIKKAN versi ini tiap kali app di-update & redeploy,
                                         // supaya SW ambil app shell versi baru (lihat activate di bawah).
 const APP_SHELL = [
   './',
